e6cb3582dc1d0b9563b7d43c09ae4207e29a838d
InfernoX21 <ayush10.pradhan@gmail.com>
2026-02-24 04:05:31 +0530

    Configure GitHub Actions deployment and static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
+    output: 'export',
+    basePath: '/IRA',
     images: {
+        unoptimized: true,
         remotePatterns: [
             {
                 protocol: 'https',
